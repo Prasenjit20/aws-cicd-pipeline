f79df1017460ca0fd85a1eca09c464c429c7d5e9
Prasenjit20 <mailtoprasenjit.biswas@gmail.com>
2021-07-03 21:24:44 +0530

    change in state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-        bucket = "statefile-cicd-pipeline"
+        bucket = "statefile-cicd-pipeline-pras"
         encrypt = true
         key = "terraform.tfstate"
         region = "ap-south-1"
